421bffd08a97f993431eef0e2ecd366f6cb27ed7
Antoine Bichat <a.bichat@yahoo.fr>
2019-12-01 09:42:12 +0100

    hex

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# adventofcode2019
+# adventofcode2019 <a href='https://adventofcode.com/2019'><img src="hex.png" align="right" height=140/></a>
 
 <!-- badges: start -->
 
---
+++ hex.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker(subplot = "favicon.png", s_x = 1, s_y = 0.8, s_width = 0.55,
+        h_fill = "#0C1021", h_color = "#FFFB7C",
+        package = "adventofcode2019", p_size = 4.5, p_color = "#FFFB7C",
+        url = "adventofcode.com/2019", u_color = "#FFFB7C", u_size = 2,
+        filename = "hex.png")
